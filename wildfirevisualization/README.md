455b6628ca64a795c4fbf62682fb14a59bda1f96
Fangnan Du <35494512+fangnandu@users.noreply.github.com>
2018-03-26 03:07:11 -0400

    Add files via upload

---
+++ wildfirevisualization/README.md
@@ -0,0 +1,9 @@
+# Mapping Higher Ed Wealth
+
+Using data from The Equality of Opportunity Project, I created a story map
+examining wealth and economic mobility among the students of every college or
+university in the U.S.
+
+View the story map [here](https://cdn.rawgit.com/simonkassel/Mapping-Higher-Ed-Wealth/6ae91564/index.html).
+
+![Example Image](https://github.com/simonkassel/Mapping-Higher-Ed-Wealth/blob/master/example.png)
---
+++ wildfirevisualization/js/sidebar_text.js
@@ -0,0 +1,7 @@
+var slide1text = "The point intensity reflects the intensity of wildfires; The color of the point reflects the intensity of the wildfires-FRP:         Fire Radiative Power (FRP) is a technique that uses remotely sensed data to quantify burned biomass. FRP measures the radiant energy released per time unit by burning vegetation.";
+
+var slide2text = "You can click on the points in the map, and it will give the following informations (1)Occurance time; (2) Day or Night;(3)FRP(intensity)"
+
+var slide3text = " In order to visualize the intensity of each fire in the Santiago aera, Using the size of the circle to represent the intensity of each fire";
+
+var slide4text = " The graduate color reflects the count number of history wildfires, which is the data from 2012-2017; Guide: by clicking the each administration, it will show the  (1)admin1 (2)admin 2 (3)admin3 (4) the counts; Based on this map, the department can distribute diffrent resources to each administration level area based on its historical wildfire data" 
